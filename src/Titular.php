a400d5d360447352bc0e9c828720fe200bd5da68
Haron Moreira <haron.moreira@surf.com.br>
2022-07-11 13:49:46 -0300

    initial commit

---
+++ src/Titular.php
@@ -0,0 +1,38 @@
+<?php
+
+class Titular
+{
+    private $cpf;
+    private $nome;
+    private $endereco;
+
+    public function __construct(CPF $cpf, string $nome, Endereco $endereco)
+    {
+        $this->cpf = $cpf;
+        $this->validaNomeTitular($nome);
+        $this->nome = $nome;
+    }
+
+    public function recuperaCpf(): string
+    {
+        return $this->cpf->recuperaNumero();
+    }
+
+    public function recuperaNome(): string
+    {
+        return $this->nome;
+    }
+
+    private function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+
+    public function getEndereco(): Endereco
+    {
+        return $this->endereco;
+    }
+}
